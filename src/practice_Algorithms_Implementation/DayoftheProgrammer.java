20fd67054947ff9eefd1d4b497c8cd632a85b300
rohit rana <er.rohitrana2013@gmail.com>
2020-04-08 22:43:15 +0530

    day of the year

---
+++ src/practice_Algorithms_Implementation/DayoftheProgrammer.java
@@ -0,0 +1,26 @@
+package practice_Algorithms_Implementation;
+
+public class DayoftheProgrammer {
+
+	// Complete the dayOfProgrammer function below.
+    static String dayOfProgrammer(int year) {
+    	String date = "";
+        if(year < 1918) {                                                   
+        	//Julian check for leap year
+            date += (year % 4 == 0) ? "12.09." + year : "13.09." + year;
+        } else if(year == 1918) {                                           
+        	//Special case: transition year
+            date += "26.09." + year;
+        } else {                                                            
+        	//Gregorian check for leap year
+            date += ((year % 400 == 0) ||                               
+            (year % 4 == 0 && year % 100 != 0)) ? "12.09." + year : "13.09." + year;
+        }
+        return date;
+     }
+
+    public static void main(String[] args) {
+        String result = dayOfProgrammer(1800);
+        System.out.println(result);
+    }
+}
